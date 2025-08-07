2c5ca6c4c793c4e08bcc5d3ffadcea801b7a9a26
jessiekeng <zixingkeng@gmail.com>
2025-08-07 13:58:37 +0800

    Add basic JS with variables and console log

---
+++ script.js
@@ -0,0 +1,10 @@
+// Variables
+let name = "Zixing";
+const age = 17;
+var isStudent = true;
+
+// Output to console
+console.log("Hello, my name is " + name);
+console.log("I am " + age + " years old.");
+console.log("Student status: " + isStudent);
+
